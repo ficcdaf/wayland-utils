4b3fb40a20a89db53cbf3450d6e888d0b5159138
Daniel Fichtinger <daniel@ficd.ca>
2025-03-16 14:49:13 -0400

    updated video compression parameters to restrict fps to 30

--- recorder/recorder.sh
+++ recorder/recorder.sh
@@ -33,7 +33,7 @@ if [[ -f "$LOCK" ]]; then
   outpath="$(compname)"
   sig "CMP"
   # compress the recording
-  ffmpeg -y -i "$RAW" -c:v libx264 -preset slow -crf 21 -b:v 2M -maxrate 3M -bufsize 4M -c:a aac -b:a 96k -movflags +faststart "$outpath" || (sig "ERR"; exit 1)
+  ffmpeg -y -i "$RAW" -c:v libx264 -preset slow -crf 21 -r 30 -b:v 2M -maxrate 3M -bufsize 4M -c:a aac -b:a 96k -movflags +faststart "$outpath" || (sig "ERR"; exit 1)
   # copy URI path to clipboard
   wl-copy -t 'text/uri-list' <<<"file://$outpath" || (sig "ERR"; exit 1)
   sig "CPD"
@@ -43,18 +43,19 @@ if [[ -f "$LOCK" ]]; then
 else
   # count how many monitors are attached
   num_mon=$(niri msg --json outputs | jq 'keys | length')
+  wf_flags="-Dyf"
   if [ "$1" = "region" ];then
     # select a screen region
     sel=$(slurp) || exit 1
-    wf-recorder -g "$sel" -Dyf "$RAW" &
+    wf-recorder -g "$sel" "$wf_flags" "$RAW" &
   elif [ "$1" = "screen" ] || [ "$1" = "" ] && (( num_mon > 1 )); then
     # select entire screen
     sel=$(slurp -o) || exit 1
-    wf-recorder -g "$sel" -Dyf "$RAW" &
+    wf-recorder -g "$sel" "$wf_flags" "$RAW" &
   else
     # this runs when screen is specified and there's only one monitor
     # it also runs with no args bc screen is default
-    wf-recorder -Dyf "$RAW" &
+    wf-recorder "$wf_flags" "$RAW" &
   fi
 
   sig "REC"
